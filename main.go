b2692d2ccca25a19e47ff99d7294f8bf4a0d2da4
chikwandagames <chikwandagames@gmail.com>
2021-02-17 18:01:47 +0000

    basic slice and for loop

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+// go Array == java Array
+// go Slice == java Arraylist
+
+func main() {
+	// Slice
+	cards := []string{"Ace of Diamonds", newCard(), newCard()}
+	// Append returns a new slice
+	cards = append(cards, "Six of spades")
+
+	// := because we reinitialize the value of i and card with each iteration
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
