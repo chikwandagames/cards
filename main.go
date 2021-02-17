2ae479232de5510e69e7ff50b81a522d68eff861
chikwandagames <chikwandagames@gmail.com>
2021-02-17 16:07:36 +0000

    strings

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Long form of varibale declaration
+	var aStr string = "A random string"
+	fmt.Println(aStr)
+
+	// Short form, using the type of declaration
+	// the compiler inferes the variable type
+	// := is only used to define new variables
+	cards := "Ace of Spaceds"
+	fmt.Println(cards)
 }
