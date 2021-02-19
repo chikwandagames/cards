7e22fabb2e22cd17575de9e544871a808bd2964f
chikwandagames <chikwandagames@gmail.com>
2021-02-19 13:24:45 +0000

    created a custom type deck

--- deck.go
+++ deck.go
@@ -1,3 +1,22 @@
 package main
 
+import "fmt"
+
 // A custom type extends a base type and adds some funtionality to it
+
+// Create an new type deck, which is a slice of strings
+// type deck == []string
+type deck []string
+
+// (d deck) is a receiver, i.e.
+// Any variable of type deck now has access to myPrint method
+// d is the current deck item we are working with, remember d is []string
+
+// Calling myPrint on an item of type deck, will pass that item as a receiver
+// then print it using the for loop
+
+func (d deck) myPrint() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cards := []string{"Ace if Diamonds", newCard()}
+	cards2 := deck{"Jack of Diamonds", newCard()}
+
+	fmt.Println(cards)
+
+	cards2.myPrint()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
