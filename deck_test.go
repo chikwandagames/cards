a59dd9402b25d72861298971bee1e7f5eb7cc87e
chikwandagames <chikwandagames@gmail.com>
2021-02-20 13:43:17 +0000

    file io testing

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	// check the deck has x number of cards
@@ -9,4 +12,30 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 20 {
 		t.Errorf("Expected deck of len 20, but got: %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spade, but go: %v", d[0])
+	}
+
+	if d[len(d)-1] != "Five of Clubs" {
+		t.Errorf("Expected last card of Five of Clubs but got: %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	// Start by delete deck testing text file if exists
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	// Retrieve file
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Expected len of 20 but got: %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+
 }
