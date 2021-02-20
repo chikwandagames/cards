6643024d768a4ecb48d4734f27ec7cd860431849
chikwandagames <chikwandagames@gmail.com>
2021-02-20 14:36:20 +0000

    created struct basic outline

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+type person struct {
+	// Properties
+	firstName string
+	lastName  string
+}
+
 func main() {
+	// Declration
+	// option 1
+	alex := person{"Alex", "Anderson"}
 
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	// option 2
+	bob := person{firstName: "Bob", lastName: "Marley"}
 
+	// option 2
+
+	fmt.Println(alex)
+	fmt.Println(bob)
 }
