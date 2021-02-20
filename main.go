4ec44932d29369a332ae26b34facf03d3b97a9f9
chikwandagames <chikwandagames@gmail.com>
2021-02-20 15:23:23 +0000

    struct declaration and initialisation

--- main.go
+++ main.go
@@ -16,7 +16,16 @@ func main() {
 	// option 2
 	bob := person{firstName: "Bob", lastName: "Marley"}
 
-	// option 2
+	// option 3
+	var jane person
+	// At this point firstName and lastName are zero values
+	// Defult values e.g. int 0, bool false, string ""...
+	fmt.Println(jane)
+	// %+v, will print all the fields and their values
+	fmt.Printf("%+v \n", jane)
+	//
+	jane.firstName = "Jane"
+	jane.lastName = "Thomas"
 
 	fmt.Println(alex)
 	fmt.Println(bob)
