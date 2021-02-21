5debc0aacdd8c270eaf5175f1a3f6f89c185161e
chikwandagames <chikwandagames@gmail.com>
2021-02-21 21:04:30 +0000

    adding deleting iterating maps

--- main.go
+++ main.go
@@ -5,24 +5,26 @@ import (
 )
 
 func main() {
-
-	// option 1
 	// Map with keys of type string and values of type string
 	colors := map[string]string{
 		"red":   "ff0000",
 		"blue":  "00ff00",
 		"green": "0000ff",
+		"white": "ffffff",
+		"black": "000000",
 	}
+	colors["pink"] = "fff050"
 
 	fmt.Println(colors)
+	// Deleting a key and value
+	delete(colors, "red")
+	fmt.Println(colors)
 
-	// option 2
-	var animals map[string]string
-	fmt.Println(animals)
+	printMap(colors)
+}
 
-	// option 3
-	trees := make(map[string]string)
-	trees["mahogany"] = "Zimbabwe"
-	fmt.Println(trees)
-
+func printMap(cm map[string]string) {
+	for k, v := range cm {
+		fmt.Println(k + ": " + v)
+	}
 }
