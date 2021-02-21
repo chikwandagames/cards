29d37432ee085323c67ca0101b7762d86b462bb3
chikwandagames <chikwandagames@gmail.com>
2021-02-21 15:15:28 +0000

    map definition

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	// option 1
+	// Map with keys of type string and values of type string
+	colors := map[string]string{
+		"red":   "ff0000",
+		"blue":  "00ff00",
+		"green": "0000ff",
+	}
+
+	fmt.Println(colors)
+
+	// option 2
+	var animals map[string]string
+	fmt.Println(animals)
+
+	// option 3
+	trees := make(map[string]string)
+	trees["mahogany"] = "Zimbabwe"
+	fmt.Println(trees)
 
 }
