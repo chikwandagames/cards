0e31754b6c29f127d219e3fa35c8b5153ba3dbeb
chikwandagames <chikwandagames@gmail.com>
2021-02-22 15:15:59 +0000

    interface declaration and definition

--- main.go
+++ main.go
@@ -2,10 +2,48 @@ package main
 
 import "fmt"
 
+// Interfaces are named collections of method signatures.
+
+// type bot interface {},
+// this means, any type that implements the method getGreeting is now of type
+// bot as well
+// therefore any value of type spanishBot or englishBot can now be passed to
+// functions that accept type bot
+// bot is of interface type, therefore we cannot directly create a value of bot
+// spanishBot, englishBot, int, map, struct, string... are concrete types
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct {
+}
+
+type spanishBot struct {
+}
+
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	printGreeting(eb)
+	printGreeting(sb)
+}
 
+func (eb englishBot) getGreeting() string {
+	/*
+		Custom logic for genetation english greeting here
+	*/
+	return "Hi there!"
+}
+
+// I we are not using the spanish bot variable, we can leave it out completely
+func (spanishBot) getGreeting() string {
+	/*
+		Custom logic for genetation spanish greeting here
+	*/
+	return "Holla!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
