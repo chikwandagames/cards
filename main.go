ae110a6dc5cd8f5bbf6afb110fc49fc7d57b1698
chikwandagames <chikwandagames@gmail.com>
2021-02-22 17:25:08 +0000

    Read function

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	// the byte slice for holding all the data added to it by the Read function
+	// 999999 is the size or length
+	bSlice := make([]byte, 99999)
+
+	resp.Body.Read(bSlice)
+	fmt.Println(string(bSlice))
 
 }
